6a8f3bbae0411337aa44c8cc65d8f0b71c7b6018
tumael <tumael.suscripciones@gmail.com>
2019-09-26 11:14:02 -0400

    Update the uploadFile method

--- src/main/java/com/jalasoft/ocrwebservice/controller/FileController.java
+++ src/main/java/com/jalasoft/ocrwebservice/controller/FileController.java
@@ -1,19 +1,13 @@
 package com.jalasoft.ocrwebservice.controller;
 
-import com.jalasoft.ocrwebservice.payload.UploadFileResponse;
+import com.jalasoft.ocrwebservice.exception.LanguageException;
 import com.jalasoft.ocrwebservice.service.FileStorageService;
+import com.jalasoft.ocrwebservice.util.Language;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @RestController
 public class FileController {
@@ -24,39 +18,16 @@ public class FileController {
     private FileStorageService fileStorageService;
 
     @PostMapping("/uploadFile")
-    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = fileStorageService.storeFile(file);
+    public String uploadFile(@RequestParam("file") MultipartFile file,
+                             @RequestParam("lang") String lang) {
+        // Verifying the correct language
+        if (lang.equalsIgnoreCase(Language.EN.getAbbreviation()) ||
+                lang.equalsIgnoreCase(Language.ES.getAbbreviation())) {
+            String fileName = fileStorageService.storeFile(file);
 
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(fileName)
-                .toUriString();
-
-        return new UploadFileResponse(fileName, fileDownloadUri,
-                file.getContentType(), file.getSize());
-    }
-
-    @GetMapping("/downloadFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
-
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
+            return String.format("The '%s' file was uploaded successfully.", fileName);
+        } else {
+            throw new LanguageException(String.format("The '%s' language isn't supported.", lang));
         }
-
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
     }
 }
